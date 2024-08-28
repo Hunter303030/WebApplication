4dadff807a62c758c0e03896000e76fdd052764e
Олег Друян <druian.oleg@yandex.ru>
2024-08-28 21:21:34 +0300

    Сделан первоначальный макет layout

---
+++ wwwroot/js/menu.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.querySelector('.menu');
+    const menuIcon = document.querySelector('.menu-icon');
+
+    menuIcon.addEventListener('click', function (e) {
+        e.preventDefault();
+        menu.classList.toggle('open');
+    });
+
+    menuIcon.addEventListener('click', function (e) {
+        e.preventDefault();
+        menu.classList.toggle('close');
+    });
+});
