6bec4507411dddc9817fc9d134d2db2239fd9c9e
Олег Друян <druian.oleg@yandex.ru>
2024-08-31 15:28:43 +0300

    Метод вызова представления для авторизации и метод обработки авторизации

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Dto;
+using WebApplication.Repositories.Interfaces;
 
 namespace WebApplication.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public IActionResult AuthView()
         {
             return View("~/Views/User/Auth.cshtml");
+        }
+
+        public IActionResult Auth(ProfileAuthDto profileAuthDto)
+        {
+            return View();
         }
     }
 }
