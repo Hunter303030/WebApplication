ca61ec0633a0533ee05eb03c8ed715e0f232f86b
Олег Друян <druian.oleg@yandex.ru>
2024-09-09 11:45:18 +0300

    У курсов появилась цена

--- Models/Course.cs
+++ Models/Course.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Discription { get; set; }
+        public int Price { get; set; }
         public DateTime DateCreate { get; set; }
         public DateTime DateUpdata { get; set; }
         public string PreviewUrl { get; set; }
