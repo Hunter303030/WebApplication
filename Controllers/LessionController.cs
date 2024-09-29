fdd329165bbcde5c4042f23c04e51a98296e915c
Олег Друян <druian.oleg@yandex.ru>
2024-09-29 20:43:22 +0300

    Форма для вывода списка уроков

---
+++ Controllers/LessionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.ViewsPath;
+
+namespace WebApplication.Controllers
+{
+    [Authorize]
+    public class LessionController : Controller
+    {
+        public IActionResult ListLessionView(Guid courseId)
+        {
+            return View(ViewPaths.Lession.ListContor);
+        }
+    }
+}
--- ViewsPath/ViewsPath.cs
+++ ViewsPath/ViewsPath.cs
@@ -17,6 +17,11 @@
             public const string EditPassword = "~/Views/Profile/EditPassword.cshtml";
             public const string Register = "~/Views/Profile/Register.cshtml";
         }
-    }
 
+        public static class Lession
+        {
+            public const string ListContor = "~/Views/Lession/ListControl.cshtml"; 
+            public const string Add = "~/Views/Lession/Add.cshtml"; 
+        }
+    }
 }
